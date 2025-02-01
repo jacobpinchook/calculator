351ea6616bed6fa1978d028f3c3afc19da0f3dba
Jacob Pinchook <jacob.pinchook@gmail.com>
2025-02-01 16:23:13 -0500

    Finished calculator tutorial

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
-// Main operator setup
-const firstNum = '';
-const operator = '';
-const secondNum = '';
 
-function operate(a, b, op) {
-    if (op == '+') return add(a, b);
-    else if (op == '-') return subtract(a, b);
-    else if (op == '*') return multiply(a, b);
-    else if (op == '/') return divide(a, b);
-}
+// Populate the display with input
+const inputValue = document.getElementById("user-input");
 
-// Calculation functions
-function add(a, b) {
-    const sum = a + b;
-    return sum;
-}
+const number = document.querySelectorAll(".numbers").forEach(function (item) {
+    item.addEventListener("click", function (e) {
+    if (inputValue.innerText === "NaN") {
+        inputValue.innerText = "";
+    }
+    if (inputValue.innerText === "0") {
+        inputValue.innerText = "";
+    }
+    inputValue.innerText += e.target.innerHTML.trim();
+    });
+});
 
-function subtract(a, b) {
-    const subtrahend = a - b;
-    return subtrahend;
-}
+const calculate = document
+  .querySelectorAll(".operations")
+  .forEach(function (item) {
+    item.addEventListener("click", function (e) {
+        let lastValue = inputValue.innerText.substring(inputValue.innerText.length, inputValue.innerText.length - 1);
 
-function multiply(a, b) {
-    const product = a * b;
-    return product;
-}
+        if (!isNaN(lastValue) && e.target.innerHTML === "=") {
+            inputValue.innerText = eval(inputValue.innerText);
+        } else if (e.target.innerHTML === "AC") {
+            inputValue.innerText = 0;
+        } else if (e.target.innerHTML === "DEL") {
+            inputValue.innerText = inputValue.innerText.substring(0,inputValue.innerText.length - 1);
+            if (inputValue.innerText.length == 0) {
+                inputValue.innerText = 0;
+            }
+        } else {
+            if (!isNaN(lastValue)) {
+                inputValue.innerText += e.target.innerHTML;
+            }
+        }
+    });
+});
 
-function divide(a, b) {
-    const remainder = a / b;
-    return remainder;
-}
